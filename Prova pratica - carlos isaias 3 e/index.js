9ea49858fee34ba81d56f9f61cba478656383d95
Carlos Isaias <109426872+Carlosisaias0012345@users.noreply.github.com>
2023-07-10 12:35:39 -0300

    Add files via upload

---
+++ Prova pratica - carlos isaias 3 e/index.js
@@ -0,0 +1,48 @@
+// atividade 01
+
+// const http = require('http')
+// const PORT = 3000
+// const server = http.createServer((req,res)=>{
+//   res.writeHead(200, ('Content-type', 'application/json'))
+//   res.write('json')
+//   res.end
+// })
+// console.log(json)
+
+// atividade 02
+
+// const address = “https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf”
+// const link = new url(address)
+
+// console.log(addresshost)
+
+// atividade 03
+
+// atividade 04
+
+// const fs = require('fs')
+// const arqAntigo = 'arquivo.txt'
+// const arqNovo = 'novo.txt'
+
+// fs.rename(arqAntigo, arqNovo, function(err){
+//   if(err){
+//       console.log(err)
+//        return
+//    }
+//   console.log(`O arquivo ${arqAntigo} foi renomeado para ${arqNovo}`)
+// })
+
+// atividade 05
+
+// const url = require('url')
+// var utf8 = require('utf8')
+
+// const adress = 'https://www.meusite,=.com.br/catalog?produtos=cadeira'
+
+// const parseUrl = new url.URL(adress)
+
+// console.log(parseUrl.host)
+// console.log(parseUrl.pathname)
+// console.log(parseUrl.search)
+// console.log(parseUrl.searchParams)
+// console.log(parseUrl.searchParams.get('produtos'))
